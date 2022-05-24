282caa1a6930f9007c446bfe4ac41d1ba2d600de
DivakarTS567 <divakar.b@tuplescale.com>
2022-05-24 18:43:35 +0530

    spring boot service with camunda dependencies added

---
+++ src/main/java/com/example/camunda/CamundaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaServiceApplication.class, args);
+	}
+
+}
