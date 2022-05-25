afab549206503f88ce052441bfaf40341b55285e
Divakar567 <divakar.budumuru567@gmail.com>
2022-05-26 00:38:50 +0530

    resilience4j retry mechanism added

--- src/main/java/com/example/camunda/consumer/InstanceEventConsumer.java
+++ src/main/java/com/example/camunda/consumer/InstanceEventConsumer.java
@@ -1,6 +1,7 @@
 package com.example.camunda.consumer;
 
 import com.example.camunda.client.MessageCorrelationClient;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.rest.dto.message.CorrelationMessageDto;
@@ -20,23 +21,27 @@ public class InstanceEventConsumer {
 
     @Async
     // @KafkaListener(topic = "instance-deployment-topic")
+    @Retry(name = "camunda-service")
     public void handleInstanceDeploymentEvent(String businessKey) throws InterruptedException {
+        log.info("Event received for {}: {}", INSTANCE_DEPLOYMENT_MESSAGE_NAME, businessKey);
         Thread.sleep(5000);
         CorrelationMessageDto correlationMessageDto = new CorrelationMessageDto();
         correlationMessageDto.setMessageName(INSTANCE_DEPLOYMENT_MESSAGE_NAME);
         correlationMessageDto.setBusinessKey(businessKey);
         messageCorrelationClient.correlateMessage(correlationMessageDto);
-        log.info("Correlate message published: {}", correlationMessageDto);
+        log.info("Correlate message published");
     }
 
     @Async
     // @KafkaListener(topic = "gwp-topic")
+    @Retry(name = "camunda-service")
     public void handleInstanceStorageEvent(String businessKey) throws InterruptedException {
+        log.info("Event received for {}: {}", INSTANCE_STORAGE_MESSAGE_NAME, businessKey);
         Thread.sleep(10000);
         CorrelationMessageDto correlationMessageDto = new CorrelationMessageDto();
         correlationMessageDto.setMessageName(INSTANCE_STORAGE_MESSAGE_NAME);
         correlationMessageDto.setBusinessKey(businessKey);
         messageCorrelationClient.correlateMessage(correlationMessageDto);
-        log.info("Correlate message published: {}", correlationMessageDto);
+        log.info("Correlate message published");
     }
 }
